30810040a7e5122d5a35df895be376e4b947fc82
James Jenkins <applesmacs@yahoo.com>
2016-05-15 15:11:44 -0400

    Clean code, add comments, process test data at end

--- multitron1.py
+++ multitron1.py
@@ -1,66 +1,98 @@
 #!/usr/bin/env python3
+"""
+    :author: James Jenkins
+"""
 
 import numpy as np
 import pickle
+import collections
+import pprint
 
+# Load little mnist data
 train, validate, test = np.array(pickle.load(open("littlemnist.pkl", "rb"), encoding = "latin1"))
 
-# Set out learning rate
-alpha = 0.5
+# Add columns of 1s to the train, validate, and test sets
+train[0] = np.c_[train[0], np.ones((len(train[0]), 1))]
+validate[0] = np.c_[validate[0], np.ones((len(validate[0]), 1))]
+test[0] = np.c_[test[0], np.ones((len(test[0]), 1))]
+
+# Set learning rate (TODO: Maybe start this higher and decay in relation to err)
+alpha = 0.4
 
 n_classes = 10
 dim_data  = len(train[0][0])
 
 # Initialize random weights (+1 to dim_data for bias)
-w = np.random.rand(n_classes, dim_data + 1)
-#w = np.zeros((n_classes, dim_data + 1))
+w = np.random.rand(n_classes, dim_data)
 
-train[0] = np.c_[train[0], np.ones((len(train[0]), 1))]
-validate[0] = np.c_[validate[0], np.ones((len(validate[0]), 1))]
+# A zero vector for comparison later
+z10 = np.zeros((n_classes,1))
 
 
-def classify(example):
+def Classify(example):
     return w.dot(example)
 
-def oneHot(index):
-    a = np.zeros((10,1))
+
+def OneHot(index, dim = 10):
+    """
+    Converts an index into a one-hot encoded column vector.
+    """
+    a = np.zeros((dim,1))
     a[index] = 1
     return a
 
+
 def Validate():
-    correct = sum((np.argmax(classify(x))) == validate[1][x_i] for x_i, x in enumerate(validate[0]))
-    print("Correct: %d"%correct)
+    """
+    Runs through all the validation data to get an error estimate.
+    """
+    correct = sum((np.argmax(Classify(x))) == validate[1][x_i] for x_i, x in enumerate(validate[0]))
+    print("Validation set correct: %d"%correct)
 
-#error = np.sum(x[:,1] - list(map(classify, x[:,0])))/len(x)
+
 Validate()
-z10 = np.zeros((10,1))
 
-iteration = 0
-while True:
-    correct = 0
-    # Enumerate training examples
-    for x_i, x in enumerate(train[0]):
-        y = classify(x)
-        d = oneHot(train[1][x_i])
+try:
+    iteration = 0
+    while True:
+        correct = 0
+        # Enumerate training examples
+        for x_i, x in enumerate(train[0]):
+            y = Classify(x)
+            d = OneHot(train[1][x_i])
 
-        # Move a proportion (alpha) of the difference between where we want to be
-        # and where we are
-        delta = d - oneHot(np.argmax(np.reshape(y, (10,1))))
-        #delta = d - np.reshape(y, (10,1))
-        w += alpha * delta * x
-        correct += np.alltrue(delta == z10)
-        #error = np.sum(x[:train_samples,1] - list(map(classify, x[:train_samples,0])))/len(x)
+            # Move a proportion (alpha) of the difference between where we want to be
+            # and where we are
+            delta = d - OneHot(np.argmax(np.reshape(y, (10,1))))
+            #delta = d - np.reshape(y, (10,1))
+            w += alpha * delta * x
+            correct += np.alltrue(delta == z10)
 
-    if iteration % 10 == 0:
-        print("Train right: %d"%correct)
-        Validate()
+        if iteration % 10 == 0:
+            print("Train right: %d"%correct)
+            Validate()
 
-    iteration += 1
+        # Break out once we achieve max on train data
+        if correct == 10000:
+            break
+
+        iteration += 1
+
+except KeyboardInterrupt:
+    print()
+
+# Calculate results on the test set
+confusion = collections.defaultdict(lambda: [0]*10)
+errors = 0
+for x_i, x in enumerate(test[0]):
+    y = np.argmax(Classify(x))
+    confusion[test[1][x_i]][y] += 1
+    errors += test[1][x_i] != y
+
+pprint.pprint(confusion)
+
+print("Test set error: %f"%(errors/len(test[0])))
 
 
-#right = 0
-#for ex in x[train_samples:]:
-#    right += classify(ex[0]) == ex[1]
-#
-#print("len(test_x) = {}".format(len(x) - train_samples))
-#print("right = {}".format(right))
+#with open("weights.bin", "wb") as f:
+#    pickle.dump(w, f)
